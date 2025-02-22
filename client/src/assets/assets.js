4e06495012c7e6824e35b2e4fcc6f3e974a243ee
DevSandeepX <sandeep87565590@gmail.com>
2025-02-22 06:05:14 +0530

    add home page

---
+++ client/src/assets/assets.js
@@ -0,0 +1,12 @@
+export const instructions = [
+    "Valid Roll Number: Only users with a valid roll number are allowed to access the exam. Roll number must be entered correctly to proceed with the login.",
+    "Login Timeout: After 15 minutes of inactivity, the user session will automatically expire, requiring re-login.",
+    "Exam Duration: The total time allotted for the DCA exam is 2 hours. Once the user starts the exam, the timer starts automatically.",
+    "Attempting Questions: Users can attempt all 100 multiple-choice questions. Once an answer is submitted, it cannot be changed.",
+    "Question Navigation: Users can navigate between questions freely, but answers must be submitted individually. A 'Mark for Review' option is available to flag questions for later review.",
+    "Randomized Question Order: The order of questions will be randomized for each user to ensure fairness.",
+    "Multiple Attempts: Each user can attempt the exam only once. After submission, users cannot retake the exam for the same session.",
+    "Scoring: Each correct answer will receive a fixed number of points (e.g., 1 point per question). Incorrect answers may not result in a penalty, but unanswered questions will not be scored.",
+    "Internet Access: Users must have a stable internet connection to take the exam. Any network disruptions may result in the exam being paused and/or invalidated.",
+    "Exam Submission: Once the user has answered all questions or the exam time expires, the exam will be automatically submitted. No changes can be made after the exam is submitted."
+];
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
-
+import React, { use, useState } from 'react'
+import { instructions } from "../assets/assets"
+import {useNavigate} from "react-router-dom"
 const Home = () => {
-  return (
-    <div className='px-32'>
+  const navigate = useNavigate()
+  const [rollnumber, setRollnumber] = useState('')
+  const [isExamStarted, setIsExamStarted] = useState(false)
+  const validNumber = 'GO123'
 
+
+  const onStartExamHandler = (e)=>{
+  e.preventDefault()
+ try {
+  if(validNumber === rollnumber){
+    navigate('/online-exam')
+  }else{
+    alert("Please fill correct roll number")
+  }
+ } catch (error) {
+  alert(error.message)
+ }
+  }
+
+
+
+  return (
+    <div className='px-32 py-8'>
+      <div>
+        <ul className='list-decimal pl-6 space-y-2'>
+          {
+            instructions.map((item, index) => (
+              <li key={index} className='my-4'>{item}</li> // Adding key to each list item
+            ))
+          }
+        </ul>
+      </div>
+
+
+      <form onSubmit={onStartExamHandler}>
+        <div className="input-box block w-[500px] mx-auto mt-12">
+          <label htmlFor="rollNumber">Roll Number</label>
+          <input
+            type="text"
+            id="rollNumber"
+            name="rollNumber"
+            value={rollnumber}
+            onChange={(e)=>setRollnumber(e.target.value)}
+            className='border border-2 border-black my-2 py-2 px-4 w-full'
+          />
+        </div>
+
+      <div className="flex justify-center mt-5">
+        <button type='submit' className='bg-blue-400 px-6 py-3 rounded text-white'>Start the Exam</button>
+      </div>
+
+      </form>
+      
     </div>
   )
 }
