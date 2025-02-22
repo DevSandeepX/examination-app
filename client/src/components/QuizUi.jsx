6ee7b1f7bf01d4a8b96f19eb02b10ea0fd158577
DevSandeepX <sandeep87565590@gmail.com>
2025-02-22 20:32:12 +0530

    Logic quiz app

--- client/src/components/QuizUi.jsx
+++ client/src/components/QuizUi.jsx
@@ -1,57 +1,84 @@
 import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import { questions } from '../assets/assets';
-
+import { useNavigate } from 'react-router-dom';
 const QuizUi = () => {
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [currentQuestion, setCurrentQuestion] = useState(questions[0]);
+const navigate = useNavigate()
+const [isExamFinished, setIsExamFinished] = useState(false)
+const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
+const [currentQuestion, setCurrentQuestion] = useState(questions[currentQuestionIndex])
+const [attempt, setAttempt] = useState([])
+const [right, setRight] = useState([])
+const [wrong, setWrong] = useState([])
+const [userSelected, setUserSelected] = useState(null)
+const [isLoading, setIsLoading] = useState(true)
 
-  // Update the current question whenever the index changes
-  useEffect(() => {
-    setCurrentQuestion(questions[currentQuestionIndex]);
-  }, [currentQuestionIndex]);
 
-  const handleNext = (e) => {
-    e.preventDefault();
-    if (currentQuestionIndex >= questions.length - 1) {
-      alert("This is the final question. Please submit your exam.");
-      return;
-    }
+const onSubmit = (e)=>{
+e.preventDefault()
+if(questions.length - 1 === currentQuestionIndex){
+  setIsExamFinished(true)
+  return null
+}else {
+if(currentQuestion.answer == userSelected){
+  setRight(prev=>[...prev, currentQuestion])
+}else{
+  setWrong(prev=>[...prev, currentQuestion])
+}
 
-    setCurrentQuestionIndex((prev) => prev + 1);
-  };
+setAttempt(prev=>[...prev, currentQuestion])
+setCurrentQuestionIndex(prev=>prev+1)
+setUserSelected(null)
+}
+
+}
+
+useEffect(()=>{
+  console.log("Attempt ",attempt.length)
+  console.log("Right ",right.length)
+  console.log("Wrong",wrong.length)
+  setCurrentQuestion(questions[currentQuestionIndex])
+},[currentQuestionIndex])
+
+
 
   return (
     <div className='min-h-screen bg-gray-100 flex flex-col items-center p-6'>
       <div className='bg-white shadow-lg rounded-xl p-6 w-full max-w-2xl mt-5'>
-        <form onSubmit={handleNext}>
+        <form onSubmit={onSubmit}>
           <h2 className='text-xl font-semibold mb-4'>
-            {currentQuestionIndex + 1}. {currentQuestion.question}
+            {currentQuestionIndex+1}. {currentQuestion.question}
           </h2>
           <div className='space-y-3'>
             <label className='flex items-center p-3 bg-gray-50 rounded-lg cursor-pointer hover:bg-gray-100'>
-              <input type='radio' name={`question${currentQuestionIndex}`} className='mr-3' />
+              <input type='radio' checked={userSelected == "1"} onChange={(e)=>setUserSelected(1)} name={`question`} className='mr-3' />
               {currentQuestion.option1}
             </label>
             <label className='flex items-center p-3 bg-gray-50 rounded-lg cursor-pointer hover:bg-gray-100'>
-              <input type='radio' name={`question${currentQuestionIndex}`} className='mr-3' />
+              <input type='radio' checked={userSelected == "2"} onChange={(e)=>setUserSelected(2)} name={`question`} className='mr-3' />
               {currentQuestion.option2}
             </label>
             <label className='flex items-center p-3 bg-gray-50 rounded-lg cursor-pointer hover:bg-gray-100'>
-              <input type='radio' name={`question${currentQuestionIndex}`} className='mr-3' />
+              <input type='radio' checked={userSelected == "3"} onChange={(e)=>setUserSelected(3)} name={`question`} className='mr-3' />
               {currentQuestion.option3}
             </label>
             <label className='flex items-center p-3 bg-gray-50 rounded-lg cursor-pointer hover:bg-gray-100'>
-              <input type='radio' name={`question${currentQuestionIndex}`} className='mr-3' />
+              <input type='radio' checked={userSelected == "4"} onChange={(e)=>setUserSelected(4)} name={`question`} className='mr-3' />
               {currentQuestion.option4}
             </label>
           </div>
-          <button
+          {
+            isExamFinished?(<button
+              type="submit"
+              onClick={()=>navigate('/finish-exam')}
+              className='mt-6 w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition' >
+              {'Finish Exam'}
+            </button>):(<button
             type="submit"
-            className='mt-6 w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition'
-          >
-            {currentQuestionIndex === questions.length - 1 ? "Submit Exam" : "Next"}
-          </button>
+            className='mt-6 w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition' >
+            {'Submit'}
+          </button>)
+          }
         </form>
       </div>
     </div>
